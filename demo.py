ad18a3044bb89b9c4bbdc6dd6ca5154bfc291792
yajit94 <112825638+yajit94@users.noreply.github.com>
2022-09-28 01:29:38 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -19,8 +19,8 @@ print('Hello %s from Jenkins %s' % (user['fullName'], version))
 # #create a blank job
 # server.create_job("job1", jenkins.EMPTY_CONFIG_XML)
 # #create pre-configured-job
-# job2_xml = open("job2.xml", mode='r', encoding='utf-8').read()
-# server.create_job("job2", job2_xml)
+job2_xml = open("job2.xml", mode='r', encoding='utf-8').read()
+server.create_job("job2", job2_xml)
 
 # job3_xml = open("job3.xml", mode='r', encoding='utf-8').read()
 # server.create_job("job3", job3_xml)
